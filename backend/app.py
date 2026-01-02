76d83c3a98dc9c4b550e46f280bcb5f41d85996e
amiralihimself <madaniamirali7@gmail.com>
2026-01-02 00:15:56 -0500

    minor typo fix

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pydantic import ValidationError
 
-from backend.schemas import RouteRequest
+from schemas import RouteRequest
 
 app = Flask(__name__)
 CORS(app)
